48469448f24bcd1a308028e6a35845b126361c38
nijie <87518156@qq.com>
2018-11-22 18:35:42 +0800

    1

--- src/main/java/com/example/deom/OrderApplication.java
+++ src/main/java/com/example/deom/OrderApplication.java
@@ -8,22 +8,23 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.metrics.atlas.EnableAtlas;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableDiscoveryClient
 @SpringBootApplication()
-@ComponentScan(basePackages = { "com.order.controller", "com.order.model", "com.order.service.impl", "my.com.base.impl" })
+@ComponentScan(basePackages = { "com.order.controller", "com.order.model", "com.order.service.impl",
+		"my.com.base.impl" })
 @MapperScan("com.order.dao")
 @EnableAspectJAutoProxy
+@EnableFeignClients(basePackages = { "com.remote.feignclient" })
 public class OrderApplication extends SpringBootServletInitializer {
 
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(OrderApplication.class, args);
 	}
-	 
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
--- src/main/java/com/order/controller/OrderController.java
+++ src/main/java/com/order/controller/OrderController.java
@@ -8,22 +8,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.order.model.OrderDetail;
 import com.order.model.OrderForm;
 import com.order.service.IOrderDetailService;
 import com.order.service.IOrderService;
+import com.remote.feignclient.IUserFeignClient;
 
 import my.com.Util.MakeOrderNum;
 
 @RestController
 @RefreshScope
 public class OrderController {
-
-	public OrderController() {
-		System.out.println("OrderController---------------------------->");
-	}
+	@Autowired
+	IUserFeignClient userFeignClient;
 
 	@Value("${demo.title}")
 	private String name;
@@ -42,6 +42,11 @@ public class OrderController {
 		System.out.println("orderDetail----------------->" + orderDetail.toString());
 		return "config" + name;
 	}
+ 
+	@RequestMapping(value="/sendhello" ,method=RequestMethod.GET)
+	public String hellofeign() {
+		return userFeignClient.findUser();
+	}
 
 	@RequestMapping("/testsend")
 	public String send1() {
@@ -54,5 +59,11 @@ public class OrderController {
 		// orderService.insert(o);
 		return "config" + name;
 	}
+	
+	@RequestMapping("/findUser")
+	public String findUser() {
+		return "findUser";
+	}
+	
 
 }
