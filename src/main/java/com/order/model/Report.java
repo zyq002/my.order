9c3b29bb7441c8eec08a22df395da799e476c76f
ni.jie <ni.jie@dr-elephant.com>
2019-08-16 17:57:52 +0800

    22

---
+++ src/main/java/com/order/model/Report.java
@@ -0,0 +1,11 @@
+package com.order.model;
+
+import lombok.Data;
+
+@Data
+public class Report {
+	private String type;
+ 	private String pdfFile;
+	private String serviceNumber;
+	private String vid;
+}
